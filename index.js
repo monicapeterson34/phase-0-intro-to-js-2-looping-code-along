305924e1fd41b800b488cd74cf6e3e0e708b45fb
Monica Peterson <monicapeterson2749@gmail.com>
2023-01-08 17:31:10 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const messages = []
+
+function writeCards(name, events) {
+    for (let i = 0; i < name.length; i++) {
+       messages.push(`Thank you, ${name[i]}, for the wonderful ${events} gift!`)}
+       console.log(messages)
+       return messages
+}
+
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
